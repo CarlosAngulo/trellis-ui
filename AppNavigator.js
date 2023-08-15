0b05efc077d3e0996b076664c8574333b58bfb48
CarlosAngulo <carlosalbertoangulomendoza@gmail.com>
2023-08-14 21:34:55 -0500

    Main navigation

--- AppNavigator.js
+++ AppNavigator.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Book Chat">
+      <Stack.Navigator initialRouteName="Main">
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="Upload PDF" component={UploadPDF} />
         <Stack.Screen name="Book Chat" component={Reader} />
